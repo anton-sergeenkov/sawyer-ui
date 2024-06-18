9f83fd9491194df1d75a8507916795608b9d09cb
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2024-06-18 10:50:25 +0400

    Bash Script Commit

--- ui-kit/index.js
+++ ui-kit/index.js
@@ -1,7 +1,13 @@
-import ButtonElement from "./ButtonElement"
-import ChipsElement from "./ChipsElement"
-import ModalElement from "./ModalElement"
-import TypographyElement from "./TypographyElement"
-import LinkElement from "./LinkElement"
+// Basic
+import ButtonElement from "./basic/ButtonElement"
+import ChipsElement from "./basic/ChipsElement"
+import ModalElement from "./basic/ModalElement"
+import TypographyElement from "./basic/TypographyElement"
+import LinkElement from "./basic/LinkElement"
 
 export { ButtonElement, ChipsElement, ModalElement, TypographyElement, LinkElement }
+
+// basic
+// layout
+// navigation
+// surfaces
