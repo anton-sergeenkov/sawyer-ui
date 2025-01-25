3f6a57c3fd36f19904baddf004c548cd7c945584
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2025-01-25 16:00:58 +0400

    ok

--- ui-kit/basic/ChipsElement/ChipsElement.tsx
+++ ui-kit/basic/ChipsElement/ChipsElement.tsx
@@ -2,6 +2,7 @@ import cn from "classnames"
 import { Fragment } from "react"
 import { checkThemeMode } from "../../utils"
 import { ITheme } from "@/sawyer-react-lib/types/common"
+import { robotoLight } from "@/sawyer-react-lib/app/fonts"
 
 import styles from "./styles.module.css"
 
@@ -27,7 +28,12 @@ const ChipsElement: React.FC<IProps> = (props) => {
       {items.map((element, index) => (
         <Fragment key={index}>
           <span
-            className={cn(styles.item, stylesTheme, className)}
+            className={cn(
+              styles.item,
+              robotoLight.className,
+              stylesTheme,
+              className,
+            )}
             style={{ userSelect: isUserSelect ? "auto" : "none" }}
           >
             {element}
