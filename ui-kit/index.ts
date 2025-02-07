cbf1f46f09a1e7c9b3b0ccf232c1d7b5474bbe69
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2025-02-08 00:05:43 +0400

    Bash Script Commit

---
+++ ui-kit/index.ts
@@ -0,0 +1,24 @@
+import UiButton, { options as UiButtonOptions } from "./basic/UiButton"
+import UiChips, { options as UiChipsOptions } from "./basic/UiChips"
+import UiLink, {
+  options as UiLinkOptions,
+  TTheme as TUiLinkTheme,
+} from "./basic/UiLink"
+
+import UiLayoutCards, {
+  options as UiLayoutCardsOptions,
+  IUiLayoutCardsItems,
+} from "./layout/UiLayoutCards"
+
+// UiButton
+export { UiButton, UiButtonOptions }
+// UiChips
+export { UiChips, UiChipsOptions }
+// UiLink
+export { UiLink, UiLinkOptions }
+export type { TUiLinkTheme }
+// UiLayoutCards
+export { UiLayoutCards, UiLayoutCardsOptions }
+export type { IUiLayoutCardsItems }
+
+// import { UiChips } from "@/sawyer-ui/ui-kit"
